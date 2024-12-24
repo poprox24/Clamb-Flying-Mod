5e598abf44823c2d822ddb1db4b1702a4770060a
Poprox24 <hudec.matyas.2006@gmail.com>
2024-12-24 14:55:46 +0100

    removed useless text

--- FlyingMod/Properties/AssemblyInfo.cs
+++ FlyingMod/Properties/AssemblyInfo.cs
@@ -5,13 +5,10 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 using MelonLoader;
 using flyingMod;
-// ...
-[assembly: MelonInfo(typeof(FlyingMod), "FlyingMod", "1.0.0", "Poprox24")]//MelonInfo(typeof(ClassOfMod), "Game Mod Name", "Version", "Author")
-[assembly: MelonGame("OP Softworks", "Clamb")] //MelonGame("Studio", "Game")]
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
+[assembly: MelonInfo(typeof(FlyingMod), "FlyingMod", "1.0.0", "Poprox24")]
+[assembly: MelonGame("OP Softworks", "Clamb")]
+
 [assembly: AssemblyTitle("FlyingMod")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
@@ -21,20 +18,9 @@ using flyingMod;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("d7f19e7a-5ede-4815-865b-65cf6bf1c0a5")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
